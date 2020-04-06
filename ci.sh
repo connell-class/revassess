a4a32d51e04eea0447590403445c9b304a6279f6
connellrobert <robert.lel.connell@gmail.com>
2020-04-05 20:40:24 -0500

    corrections

--- ci.sh
+++ ci.sh
@@ -1,4 +1,6 @@
-git checkout tier1
-cd ./RevassessTier1/
-$pass=(mvn clean test | grep PointsTests)
-echo $pass
+# git checkout tier1
+# cd ./RevassessTier1/
+# $pass=(mvn clean test | grep PointsTests)
+# echo $pass
+# git checkout master
+echo hello
