fc01ac5409739b9cf187ad8cb0b9f1065c08efe0
connellrobert <robert.lel.connell@gmail.com>
2020-05-08 13:36:06 -0400

    altering script

--- ci.sh
+++ ci.sh
@@ -1,33 +1,18 @@
 test(){
-    for i in 1 2
+    for i in 1 2 3
     do
         git checkout tier$i
         git pull
         cd ./RevassessTier$i/
-        pass="`mvn clean test | grep PointsTests`"
+        pass="`mvn clean test -Dtest=Tier${i}Tests | grep PointsTests`"
         exitCode="`echo $pass | grep -c FAILURE`"
         if (( $exitCode > 0 ))
         then
             failedTier=$i
-            break 
+            break
         fi
         cd ../
     done
-    git checkout tier3
-	git pull
-	cd RevassessTier3
-	for i in 3 4 5 
-	do
-		pass="`mvn clean test -Dtest=Tier${i}Tests`"
-		exitCode="`echo $pass | grep PointsTests | grep -c FAILURE`"
-		if (($exitCode > 0))
-		then
-			failedTier=$i
-			break
-		fi
-	done
-    mvn clean
-    cd ../
     git checkout master
 }
 test
