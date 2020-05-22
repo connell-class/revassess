d6c7df9e0f28db67035d57b247332deb453ac93c
Robert Connell <47120126+connellrobert@users.noreply.github.com>
2020-05-22 12:58:47 -0400

    Update index.js

--- RevassessTier3/src/main/webapp/js/index.js
+++ RevassessTier3/src/main/webapp/js/index.js
@@ -8,17 +8,18 @@
  *  http://ec2-3-19-123-54.us-east-2.compute.amazonaws.com:9999/flashcard
  */
 window.addEventListener('load',callFlashcardApi);
+
 let resp;
+
 //assign the returned json to the resp variable
 async function callFlashcardApi(){
 }
 
-/**
- * manipulate the dom with this function
- */
+//manipulate the dom with this function
 function manipDom(){
 }
 
+//this returns the variable that holds the json received from the api
 function getResp(){
     return resp;
 }
