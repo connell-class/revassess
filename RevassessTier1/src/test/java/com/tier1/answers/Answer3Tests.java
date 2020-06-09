1deb22a13abab71042818add88c0423c77725764
connellrobert <robert.lel.connell@gmail.com>
2020-06-09 13:34:38 -0400

    fixing tier 1 test 3 test. issue found with sum

--- RevassessTier1/src/test/java/com/tier1/answers/Answer3Tests.java
+++ RevassessTier1/src/test/java/com/tier1/answers/Answer3Tests.java
@@ -31,7 +31,7 @@ public class Answer3Tests {
         int [] arr2 = { 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12 };
         assertEquals(78, SumOverArray.IterateAndSum(arr2));
         int[] arr3 = { -1, -2, -3, -4, -5, -6, -7, -8, -9 };
-		assertEquals(45, SumOverArray.IterateAndSum(arr3));
+		assertEquals(-45, SumOverArray.IterateAndSum(arr3));
 		int[] arr4 = { 66, 67, 68, 69, 70, 71, 72, 73, 74, 75, 76, 77, 78, 79, 80 };
 		assertEquals(1095, SumOverArray.IterateAndSum(arr4));
         addPoints(30);
